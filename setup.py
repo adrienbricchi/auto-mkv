583a95470f0eb98da75e6533fb67fc175903a540
Adrien Bricchi <adrienbricchi@users.noreply.github.com>
2019-01-16 20:32:26 +0100

    Project sample

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE.md') as f:
+    license = f.read()
+
+setup(
+    name='auto-mkv',
+    version='0.1.0',
+    description='Reencoding script',
+    long_description=readme,
+    url='https://github.com/adrienbricchi/auto-mkv',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
