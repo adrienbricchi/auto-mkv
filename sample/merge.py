7eb4905d65a9e073e0cc6e4fbeec53af7873d06e
Adrien Bricchi <adrienbricchi@users.noreply.github.com>
2019-01-17 02:21:48 +0100

    Extracting audio tracks

--- sample/merge.py
+++ sample/merge.py
@@ -19,35 +19,43 @@ def parse_mkvinfo_result(lines):
     for track in tracks:
         language_info = [info for info in track if info.startswith("|  + Language: ")] + ["|  + Language: und"]
         language = language_info[0][15:]
-        media_info = [info for info in track if info.startswith("|  + Track type: ")] + ["|  + Track type: und"]
-        media = media_info[0][15:]
-        result += [(media, language)]
-    return True
+        codec_info = [info for info in track if info.startswith("|  + Codec ID: ")] + ["|  + Track type: und"]
+        codec = codec_info[0][17:].lower()
+        result += [(codec, language)]
+    return result
 
 
-def determine_audio_tracks(file_path):
+def retrieve_tracks(file_path):
     """Get an answer."""
     result = subprocess.run([mkvtoolnix_path + os.sep + "mkvinfo.exe", file_path],
                             shell=True,
                             check=False,
                             encoding='utf-8',
                             stdout=subprocess.PIPE)
-    parse_mkvinfo_result(result.stdout.split('\n'))
-    return True
+    return parse_mkvinfo_result(result.stdout.split('\n'))
 
 
-def extract(file_path, tracks):
+def extract_audio(file_path):
     """Get an answer."""
-    subprocess.run([mkvtoolnix_path + os.sep + "mkvextract.exe"], shell=True, check=True)
+    tracks = retrieve_tracks(file_path)
+    for i in range(0, len(tracks)):
+        if tracks[i][0] == "ac3" or tracks[i][0] == "dts":
+            track_name = file_path[:-4] + "_track" + str(i + 1) + "_" + tracks[i][1] + "_DELAY 0ms." + tracks[i][0]
+            if not os.path.exists(track_name):
+                subprocess.run(
+                    [mkvtoolnix_path + os.sep + "mkvextract.exe", file_path, "tracks", str(i) + ":" + track_name],
+                    shell=True,
+                    check=True
+                )
     return True
 
 
 def extract_all_in_path(folder_path):
     """Get an answer."""
-    files = glob.glob(folder_path + os.sep + "*.mkv")
-    for file in files:
-        print("File : " + file)
-        determine_audio_tracks(file)
+    files_flat = glob.glob(folder_path + os.sep + "*.mkv")
+    files_sub_folders = glob.glob(folder_path + os.sep + "**" + os.sep + "*.mkv")
+    for file in files_flat + files_sub_folders:
+        extract_audio(file)
     return True
 
 
