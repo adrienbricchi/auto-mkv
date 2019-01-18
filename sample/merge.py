2a2216752d155f0f159c4f78aba663d0df75364b
Adrien Bricchi <adrienbricchi@users.noreply.github.com>
2019-01-18 20:37:50 +0100

    Audio first draft

--- sample/merge.py
+++ sample/merge.py
@@ -8,7 +8,10 @@ import itertools
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-mkvtoolnix_path = config["DEPENDANCIES"]['mkvtoolnix_path']
+mkvextract = config["DEPENDANCIES"]['mkvtextract_path']
+mkvinfo = config["DEPENDANCIES"]['mkvinfo_path']
+eac3to = config["DEPENDANCIES"]['eac3to_path']
+neroaac = config["DEPENDANCIES"]['nero_aac_path']
 
 
 def parse_mkvinfo_result(lines):
@@ -27,11 +30,7 @@ def parse_mkvinfo_result(lines):
 
 def retrieve_tracks(file_path):
     """Get an answer."""
-    result = subprocess.run([mkvtoolnix_path + os.sep + "mkvinfo.exe", file_path],
-                            shell=True,
-                            check=False,
-                            encoding='utf-8',
-                            stdout=subprocess.PIPE)
+    result = subprocess.run([mkvinfo, file_path], shell=True, check=False, encoding='utf-8', stdout=subprocess.PIPE)
     return parse_mkvinfo_result(result.stdout.split('\n'))
 
 
@@ -42,11 +41,7 @@ def extract_audio(file_path):
         if tracks[i][0] == "ac3" or tracks[i][0] == "dts":
             track_name = file_path[:-4] + "_track" + str(i + 1) + "_" + tracks[i][1] + "_DELAY 0ms." + tracks[i][0]
             if not os.path.exists(track_name):
-                subprocess.run(
-                    [mkvtoolnix_path + os.sep + "mkvextract.exe", file_path, "tracks", str(i) + ":" + track_name],
-                    shell=True,
-                    check=True
-                )
+                subprocess.run([mkvextract, file_path, "tracks", str(i) + ":" + track_name], shell=True, check=True)
     return True
 
 
@@ -54,7 +49,23 @@ def extract_all_in_path(folder_path):
     """Get an answer."""
     files = glob.glob(folder_path + os.sep + "**" + os.sep + "*.mkv",  recursive=True)
     for file in files:
-        extract_audio(file)
+        if len(glob.glob(file + "_*")) == 0:
+            extract_audio(file)
     return True
 
+
+def reencode_audio(folder_path):
+    """Get an answer."""
+    files = glob.glob(folder_path + os.sep + "**" + os.sep + "*.ac3",  recursive=True)
+    files += glob.glob(folder_path + os.sep + "**" + os.sep + "*.dts",  recursive=True)
+    for file in files:
+        print("Reencode : " + file)
+        if len(glob.glob(file + "_*")) == 0:
+            subprocess.run([eac3to, file, "stdout.wav", "|", neroaac, "-br", " 96000", "-if", "-", "-of", file + "_096.aac"], shell=True, check=True)
+            subprocess.run([eac3to, file, "stdout.wav", "|", neroaac, "-br", "112000", "-if", "-", "-of", file + "_112.aac"], shell=True, check=True)
+            subprocess.run([eac3to, file, "stdout.wav", "|", neroaac, "-br", "128000", "-if", "-", "-of", file + "_128.aac"], shell=True, check=True)
+            subprocess.run([eac3to, file, "stdout.wav", "|", neroaac, "-br", "160000", "-if", "-", "-of", file + "_160.aac"], shell=True, check=True)
+            subprocess.run([eac3to, file, "stdout.wav", "|", neroaac, "-br", "192000", "-if", "-", "-of", file + "_192.aac"], shell=True, check=True)
+            os.remove(file)
+    return True
 
